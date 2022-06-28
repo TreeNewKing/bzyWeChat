234183c8394597b702154458c0c43a929403f280
TreeNewKing <1164175212@qq.com>
2022-06-28 18:52:30 +0800

    i

---
+++ src/test/java/com/example/qianduanlearn/QianDuanLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qianduanlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QianDuanLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
